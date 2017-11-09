e585217fa2c71d69cb78ee5868eb632519e10489
Romain Jacquier <romain.jacquier@insa-rouen.fr>
2017-11-09 16:00:22 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,49 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+  import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SelectionCafeePage } from '../pages/selection-cafee/selection-cafee';
+import { GestionUserPage } from '../pages/gestion-user/gestion-user';
+import { ApiConnectionProvider } from '../providers/api-connection/api-connection';
+import { CreateUserPopoverPage } from '../pages/gestion-user/gestion-user';
+import { ShowUserPopoverPage } from '../pages/gestion-user/gestion-user';
+import { UrlSettingsPage } from '../pages/url-settings/url-settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SelectionCafeePage,
+    GestionUserPage,
+    CreateUserPopoverPage,
+    ShowUserPopoverPage,
+    UrlSettingsPage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    BrowserModule,HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SelectionCafeePage,
+    GestionUserPage,
+    CreateUserPopoverPage,
+    ShowUserPopoverPage,
+    UrlSettingsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ApiConnectionProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SelectionCafeePage } from '../selection-cafee/selection-cafee';
+import { GestionUserPage } from '../gestion-user/gestion-user';
+import { UrlSettingsPage } from '../url-settings/url-settings';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+  }
+
+
+
+  goSelectionCaffe(){
+    this.navCtrl.push(SelectionCafeePage);
+  }
+
+  goGestionUser(){
+    this.navCtrl.push(GestionUserPage);
+  }
+
+  goUrlSettings(){
+    this.navCtrl.push(UrlSettingsPage);
   }
 
 }
